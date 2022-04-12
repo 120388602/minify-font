1b873de14ef1076c6b7c5814d8f366510b1e2614
gordonw <120388602@qq.com>
2022-04-12 16:03:03 +0800

    修改常用字体

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Fontmin = require("fontmin");
 const text = require("./chars");
 
 const fontmin = new Fontmin()
-    .src("./fonts/SourceHanSansCN-Regular.ttf")
+    .src("./fonts/DreamHanSansCN-W40.ttf")
     .use(Fontmin.glyph({ text }))
     .dest("dest");
 
